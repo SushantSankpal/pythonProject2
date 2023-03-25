57cb67776ce867a1939b598409d6b6431b4e1458
SushantSankpal <128896348+SushantSankpal@users.noreply.github.com>
2023-03-25 16:03:32 +0530

    Update main.py
    Removed unwanted variable

--- main.py
+++ main.py
@@ -8,10 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-name="Soham"
-
-print("Hi {name}")
+print("Hi Soham")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
